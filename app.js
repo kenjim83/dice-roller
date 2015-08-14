a745ddf5694228949ecbf8c45704e383f0ffeb28
kenjim83 <kenjim83@gmail.com>
2015-08-14 12:43:56 -0700

    init commit

---
+++ app.js
@@ -0,0 +1,21 @@
+/*
+Instructions
+============
+
+Let's make a dice rolling app!
+
+This app has 2 functions:
+	1. Click 'Add die!' to add new a die with a default value of "1".
+	2. Click 'Roll dice!' to roll all the dice and display a number between "1" and "6".
+
+
+Hints:
+- Watch the preview.mov file to see how the final product might look.
+- Feel free to use (or not use) jQuery. It's already included in app.html
+- There's already a styled 'die' class in app.html
+
+Bonus:
+
+Add an output below which displays the sum of all the dice
+
+*/
